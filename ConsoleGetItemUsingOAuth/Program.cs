b047cedd7df3e6a0b63b556894cc2d8e188566c1
Gunther Kroth <gdk6217@rit.edu>
2020-05-07 23:01:27 -0400

    Add project files.

---
+++ ConsoleGetItemUsingOAuth/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using eBay.Service.Call;
+using eBay.Service.Core.Sdk;
+using eBay.Service.Core.Soap;
+using eBay.Service.Util;
+
+namespace ConsoleGetItemUsingOAuth
+{
+    /// <summary>
+    /// A simple item adding sample,
+    /// show basic flow to list an item to eBay Site using eBay SDK.
+    /// </summary>
+    class Program
+    {
+        private static ApiContext apiContext = null;
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+                Console.WriteLine("+ Welcome to eBay SDK for .Net Sample +");
+                Console.WriteLine("+ - ConsoleGetItemUsingOAuth      +");
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+
+                //Initialize eBay ApiContext object
+                ApiContext apiContext = GetApiContext();
+
+
+                //Create Call object and execute the Call
+                GetItemCall apiCall = new GetItemCall(apiContext);
+                apiCall.ItemID = ConfigurationManager.AppSettings["ItemID"].ToString();
+                apiCall.DetailLevelList.Add(DetailLevelCodeType.ReturnAll);
+                apiCall.Execute();
+                Console.WriteLine("Begin to call eBay API, please wait ...");
+
+                Console.WriteLine("End to call eBay API, show call result ...");
+                Console.WriteLine();
+
+                //Handle the result returned
+                Console.WriteLine("ItemID: " + apiCall.Item.ItemID.ToString());                
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to get user data : " + ex.Message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Populate eBay SDK ApiContext object with data from application configuration file
+        /// </summary>
+        /// <returns>ApiContext object</returns>
+        static ApiContext GetApiContext()
+        {
+            //apiContext is a singleton,
+            //to avoid duplicate configuration reading
+            if (apiContext != null)
+            {
+                return apiContext;
+            }
+            else
+            {
+                apiContext = new ApiContext();
+
+                //set Api Server Url
+                apiContext.SoapApiServerUrl =
+                    ConfigurationManager.AppSettings["Environment.ApiServerUrl"];
+                //set Api Token to access eBay Api Server
+                ApiCredential apiCredential = new ApiCredential();
+                apiCredential.oAuthToken =
+                    ConfigurationManager.AppSettings["UserAccount.ApiToken"];
+                apiContext.ApiCredential = apiCredential;
+                //set eBay Site target to US
+                apiContext.Site = SiteCodeType.US;
+
+
+                //set Api logging
+                apiContext.ApiLogManager = new ApiLogManager();
+                apiContext.ApiLogManager.ApiLoggerList.Add(
+                    new FileLogger("listing_log.txt", true, true, true)
+                    );
+                apiContext.ApiLogManager.EnableLogging = true;
+
+
+                return apiContext;
+            }
+        }
+
+
+
+    }
+}
